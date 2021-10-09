52261b48d59b2e4f8e2f7db3dcb912e266cbc3fe
samba sallah <sambasalalh10@gmail.com>
2021-10-09 14:46:43 -0500

    Revert "Final Commit"
    This reverts commit 8ee79b9d51a4ed623cf03e81388d432839cf66c5.

---
+++ src/librarysystem/LibrarySystem.java
@@ -0,0 +1,272 @@
+package librarysystem;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import business.ControllerInterface;
+import business.SystemController;
+import dataaccess.Auth;
+
+
+public class LibrarySystem extends JFrame implements LibWindow {
+
+	ControllerInterface ci = new SystemController();
+	public final static LibrarySystem INSTANCE =new LibrarySystem();
+	JPanel mainPanel;
+	JMenuBar menuBar;
+    JMenu options;
+    JMenuItem login, allBookIds, allMemberIds, addLibraryMember, checkout, logout; 
+    String pathToImage;
+    private boolean isInitialized = false;
+    
+    private static LibWindow[] allWindows = { 
+    	LibrarySystem.INSTANCE,
+		LoginWindow.INSTANCE,
+		AllMemberIdsWindow.INSTANCE,	
+		AllBookIdsWindow.INSTANCE,
+		AddLibraryMember.INSTANCE,
+		Checkout.INSTANCE,
+		CheckoutHistory.INSTANCE
+	};
+        	
+    	
+	public static void hideAllWindows() {
+		
+		for(LibWindow frame: allWindows) {
+			frame.setVisible(false);
+		}
+		
+	}
+    
+    
+    private LibrarySystem() {}
+    
+    public void init() {
+    	formatContentPane();
+    	setPathToImage();
+    	insertSplashImage();
+		
+		createMenus();
+		pack();
+		setSize(660,500);
+		isInitialized = true;
+    }
+    
+    private void formatContentPane() {
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout(1,1));
+		getContentPane().add(mainPanel);	
+	}
+    
+    private void setPathToImage() {
+    	String currDirectory = System.getProperty("user.dir");
+    	pathToImage = currDirectory+"//src//librarysystem//library.jpg";
+    }
+    
+    private void insertSplashImage() {
+        ImageIcon image = new ImageIcon(pathToImage);
+		mainPanel.add(new JLabel(image));	
+    }
+    private void createMenus() {
+    	menuBar = new JMenuBar();
+		menuBar.setBorder(BorderFactory.createRaisedBevelBorder());
+		addMenuItems();
+		setJMenuBar(menuBar);		
+    }
+    
+    private void addMenuItems() {
+       options = new JMenu("Options");  
+ 	   menuBar.add(options);
+ 	   login = new JMenuItem("Login");
+ 	   login.addActionListener(new LoginListener());
+ 	   allBookIds = new JMenuItem("All Book Ids");
+ 	   allBookIds.addActionListener(new AllBookIdsListener());
+ 	   allMemberIds = new JMenuItem("All Member Ids");
+ 	   allMemberIds.addActionListener(new AllMemberIdsListener());
+ 	    addLibraryMember = new JMenuItem("Add Library Member");
+ 	    addLibraryMember.addActionListener(new AddLibraryMemberListener());
+ 	   checkout = new JMenuItem("Checkout book");
+	    checkout.addActionListener(new CheckoutListener());
+	    logout = new JMenuItem("Logout");
+	    logout.addActionListener(new LogoutListener());
+ 	  if(SystemController.currentAuth == null)  options.add(login);
+ 	   options.add(allBookIds);
+ 	   options.add(allMemberIds);
+ 	   options.add(addLibraryMember);
+ 	   options.add(checkout);
+ 	   if(SystemController.currentAuth != null) options.add(logout);
+
+    }
+    
+    class LoginListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			LibrarySystem.hideAllWindows();
+			LoginWindow.INSTANCE.init();
+			Util.centerFrameOnDesktop(LoginWindow.INSTANCE);
+			LoginWindow.INSTANCE.setVisible(true);
+			
+		}
+    	
+    }
+    
+    class LogoutListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			SystemController.currentAuth = null;
+			LibrarySystem.hideAllWindows();
+			LoginWindow.INSTANCE.init();
+			Util.centerFrameOnDesktop(LoginWindow.INSTANCE);
+			LoginWindow.INSTANCE.setVisible(true);
+			
+		}
+    	
+    }
+    
+    class AllBookIdsListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			if(SystemController.currentAuth == null) {
+    			JOptionPane.showMessageDialog(LibrarySystem.this, "You have to login first");
+    			return;
+    		};
+			
+			LibrarySystem.hideAllWindows();
+			AllBookIdsWindow.INSTANCE.init();
+			
+			List<String> ids = ci.allBookIds();
+			Collections.sort(ids);
+			StringBuilder sb = new StringBuilder();
+			for(String s: ids) {
+				sb.append(s + "\n");
+			}
+			System.out.println(sb.toString());
+			AllBookIdsWindow.INSTANCE.setData(sb.toString());
+			AllBookIdsWindow.INSTANCE.pack();
+			//AllBookIdsWindow.INSTANCE.setSize(660,500);
+			Util.centerFrameOnDesktop(AllBookIdsWindow.INSTANCE);
+			AllBookIdsWindow.INSTANCE.setVisible(true);
+			
+		}
+    	
+    }
+    
+    class AllMemberIdsListener implements ActionListener {
+
+    	@Override
+		public void actionPerformed(ActionEvent e) {
+    		
+    		if(SystemController.currentAuth == null){
+    			JOptionPane.showMessageDialog(LibrarySystem.this, "You have to login first");
+    			return;
+    		};
+    		
+			LibrarySystem.hideAllWindows();
+			AllMemberIdsWindow.INSTANCE.init();
+			AllMemberIdsWindow.INSTANCE.pack();
+			AllMemberIdsWindow.INSTANCE.setVisible(true);
+			
+			
+			LibrarySystem.hideAllWindows();
+			AllBookIdsWindow.INSTANCE.init();
+			
+			List<String> ids = ci.allMemberIds();
+			Collections.sort(ids);
+			StringBuilder sb = new StringBuilder();
+			for(String s: ids) {
+				sb.append(s + "\n");
+			}
+			System.out.println(sb.toString());
+			AllMemberIdsWindow.INSTANCE.setData(sb.toString());
+			AllMemberIdsWindow.INSTANCE.pack();
+			//AllMemberIdsWindow.INSTANCE.setSize(660,500);
+			Util.centerFrameOnDesktop(AllMemberIdsWindow.INSTANCE);
+			AllMemberIdsWindow.INSTANCE.setVisible(true);
+			
+			
+		}
+    	
+    }
+    
+    class AddLibraryMemberListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			if(SystemController.currentAuth == null) {
+    			JOptionPane.showMessageDialog(LibrarySystem.this, "You have to login first");
+    			return;
+    		};
+			
+			if((SystemController.currentAuth != Auth.ADMIN)) {
+    			JOptionPane.showMessageDialog(LibrarySystem.this, "You not an ADMIN");
+    			return;
+    		};
+			
+			LibrarySystem.hideAllWindows();
+			AddLibraryMember.INSTANCE.init();
+			AddLibraryMember.INSTANCE.pack();
+			AddLibraryMember.INSTANCE.setVisible(true);
+			
+			
+			LibrarySystem.hideAllWindows();
+			AddLibraryMember.INSTANCE.init();
+			
+
+			Util.centerFrameOnDesktop(AddLibraryMember.INSTANCE);
+			AddLibraryMember.INSTANCE.setVisible(true);
+		}
+    	
+    }
+    
+    
+    class CheckoutListener implements ActionListener {
+
+    	@Override
+		public void actionPerformed(ActionEvent e) {
+    		
+    		if(SystemController.currentAuth == null) {
+    			JOptionPane.showMessageDialog(LibrarySystem.this, "You have to login first");
+    			return;
+    		};
+    		
+			LibrarySystem.hideAllWindows();
+			Checkout.INSTANCE.init();
+			Util.centerFrameOnDesktop(Checkout.INSTANCE);
+			Checkout.INSTANCE.setVisible(true);
+			
+		}
+      	
+      }
+
+
+	@Override
+	public boolean isInitialized() {
+		return isInitialized;
+	}
+
+
+	@Override
+	public void isInitialized(boolean val) {
+		isInitialized =val;
+		
+	}
+    
+}
